d8c5fc912badecee06fc8194ad5e9f5c7f48c194
xiexuyuyan <2351783158@qq.com>
2021-11-04 14:53:46 +0800

    0.0.2 在rt setting打开 usb cdc

--- rtconfig.h
+++ rtconfig.h
@@ -93,6 +93,17 @@
 
 /* Using USB */
 
+#define RT_USING_USB_DEVICE
+#define RT_USBD_THREAD_STACK_SZ 4096
+#define USB_VENDOR_ID 0x0FFE
+#define USB_PRODUCT_ID 0x0001
+#define _RT_USB_DEVICE_CDC
+#define RT_USB_DEVICE_CDC
+#define RT_VCOM_TASK_STK_SIZE 512
+#define RT_CDC_RX_BUFSIZE 128
+#define RT_VCOM_SERNO "32021919830108"
+#define RT_VCOM_SER_LEN 14
+#define RT_VCOM_TX_TIMEOUT 1000
 /* end of Using USB */
 /* end of Device Drivers */
 
